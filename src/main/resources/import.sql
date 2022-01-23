67af899cdd2ed792a9180df3bce321103b2549b3
Gabriel Nunes <74620108+gabrielnuunes@users.noreply.github.com>
2022-01-23 09:35:52 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,8 @@
-insert into post (id, titulo, nome, data, tag, texto) values ('1', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
-insert into post (id, titulo, nome, data, tag, texto) values ('2', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
-insert into post (id, titulo, nome, data, tag, texto) values ('3', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
-insert into post (id, titulo, nome, data, tag, texto) values ('4', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
-insert into post (id, titulo, nome, data, tag, texto) values ('5', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
-insert into post (id, titulo, nome, data, tag, texto) values ('6', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
-insert into post (id, titulo, nome, data, tag, texto) values ('7', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Desenvolvedor', '981029127');
+insert into post (id, titulo, nome, data, tag, texto) values ('1', 'Gabriel Nunes', 'gabrielnuunes22@gmail.com', '1993-02-01', 'Sservicop', '981029127');
+insert into post (id, titulo, nome, data, tag, texto) values ('2', 'Fulano Silva', 'fulanosilva@gmail.com', '1993-02-01', 'Trabalhador', '111222');
+insert into post (id, titulo, nome, data, tag, texto) values ('3', 'Ciclaro Ferreira', 'emailcom', '1993-02-01', 'trabalhador', '981029127');
+insert into post (id, titulo, nome, data, tag, texto) values ('4', 'Maria otavio', 'maria@gmail.com', '1993-02-01', 'trabalhador', '981029127');
+insert into post (id, titulo, nome, data, tag, texto) values ('5', 'Jorge Lucas', 'jorge@gmail.com', '1993-02-01', 'trabaladr', '981029127');
+insert into post (id, titulo, nome, data, tag, texto) values ('6', 'felix te', 'gabrielnuunes22@gmail.com', '1993-02-01', 'trabalhador', '981029127');
+insert into post (id, titulo, nome, data, tag, texto) values ('7', 'Douglas sousa', 'gabrielnuunes22@gmail.com', '1993-02-01', 'trabalhador', '981029127');
+
